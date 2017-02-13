410c4ff74b22ecd82719fb863fea4c6ff40124f2
纳兰天忆 <103520146@qq.com>
2017-02-13 21:44:20 +0800

    //

--- 04-dev-server/src/App.js
+++ 04-dev-server/src/App.js
@@ -7,6 +7,6 @@ export default class App extends Component { // eslint-disable-line
   render() {
     // console.log('Source Map Testing');
     // throw new Error();
-    return <h1>hello world</h1>;
+    return <h1>hello world 小鸡</h1>;
   }
 }
