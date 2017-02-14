c75c3127347650d8a686e0d9c235025d45568bf5
纳兰天忆 <103520146@qq.com>
2017-02-14 14:08:27 +0800

    //给store默认的state

--- 19-1-isomorphic-server/index.js
+++ 19-1-isomorphic-server/index.js
@@ -1,7 +1,16 @@
 /* eslint-disable */
+
+//该例子需要手动创建webpack-assets.json
+/*
+ {
+ "assets":{
+ "./Counter.png":"/dist/Counter.png"
+ }
+ }
+ */
 var WebpackIsomorphicTools = require('webpack-isomorphic-tools');
 
-var config = { assets: { images: { extensions: ['png'] } } };
+var config = {assets: {images: {extensions: ['png']}}};
 
 new WebpackIsomorphicTools(config)
   .development()
